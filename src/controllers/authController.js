e8e64536986b2e1cf20002f39e191fcbc662f92e
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-02-15 10:41:34 +0330

    add jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,17 @@
+const jwt = require("jsonwebtoken");
+
 const authController = {
   Login: async (req, res) => {
     const { email, password } = req.body;
-
     try {
       if (email === "admin@gmail.com" && password === "admin") {
+        const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
         res.json({
           success: true,
           message: "Login successful",
+          token,
         });
       } else {
         res.status(401).json({
